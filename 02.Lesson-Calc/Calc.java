ae2db48b0fb173d7029b8928592c1655cc628616
avdok <a.v.dokuchaev@gmail.com>
2021-06-05 15:01:59 +0300

    new version

--- 02.Lesson-Calc/Calc.java
+++ 02.Lesson-Calc/Calc.java
@@ -1,19 +1,83 @@
+import java.util.Scanner;
+ 
 public class Calc {
+    static Scanner scanner = new Scanner(System.in);
 
-	public static void main(String args[]) {
+    public static void main(String[] args) {
+        
+        double num1, num2, result;
+        char operation; 
+        
+        num1 = getOperand();
 
-		Operand op1 = new Operand();
-		Operand op2 = new Operand();
-		Action act =  new Action();
+	while (true) {
+            operation = getOperation();
+            if (operation == 'e' || operation == 'E') {
+                break;
+            }
+            if (operation == 'c' || operation == 'C') {
+                num1 = getOperand();
+                continue;
+            }
+            if (operation == '=') {
+                System.out.println("Результат = " + num1);
+                result = num1;
+                continue;
+            }
 
-		while (true)
-		{
-			op1.getInput();
-			act.getInput();
-			if (act.stopped()) break;
-			act.execute(op1);
-		}
-
-
-	}
+            num2 = getOperand();
+            result = calc(num1, num2, operation);
+            System.out.println("промежуточный результат " + num1 + " " + operation + " " + num2 + " = " + result);
+            num1 = result;
+            
+        }
+    }
+ 
+    public static double getOperand(){
+        System.out.println("Введите число:");
+        double num;
+        if(scanner.hasNextDouble()){
+            num = scanner.nextDouble();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();//рекурсия
+            num = getOperand();
+        }
+        return num;
+    }
+ 
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();//рекурсия
+            operation = getOperation();
+        }
+        return operation;
+    }
+ 
+    public static double calc(double num1, double num2, char operation){
+        double result;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
 }
