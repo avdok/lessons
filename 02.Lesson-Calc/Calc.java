3452a8952ac3ade421062a5c7bdb2c5486ff4e4f
avdok <a.v.dokuchaev@gmail.com>
2021-06-06 16:34:32 +0300

    Add exception

--- 02.Lesson-Calc/Calc.java
+++ 02.Lesson-Calc/Calc.java
@@ -60,7 +60,7 @@ public class Calc {
     }
  
     public static double calc(double num1, double num2, char operation){
-        double result;
+        double result = 0;
         switch (operation){
             case '+':
                 result = num1+num2;
@@ -72,7 +72,15 @@ public class Calc {
                 result = num1*num2;
                 break;
             case '/':
-                result = num1/num2;
+                try {
+                    result = num1/num2;
+                    if (result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY) {
+                        throw new ArithmeticException();
+                    }
+                }
+                catch (ArithmeticException ae) {
+                    System.out.println("Деление на ноль!");
+                }
                 break;
             default:
                 System.out.println("Операция не распознана. Повторите ввод.");
